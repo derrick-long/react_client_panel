54cf8173eaa9e09fd0707566d690800330bbeb00
derrick-long <long.derrick8@gmail.com>
2019-01-17 23:39:06 -0500

    start client add feature

---
+++ src/components/clients/AddClient.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+class AddClient extends Component {
+    state = {
+        firstName: '',
+        lastname: '',
+        email: '',
+        phone: '',
+        balance: ' '
+    }
+  render() {
+    return (
+      <div>
+          <div className="row">
+            <div className="col-md-6">
+                <Link to="/" className="btn btn-link">
+                    <i className="fas fa-arrow-circle-left"></i>Back To Dashboard
+                </Link>
+            </div>
+        </div>
+
+        <div className="card">
+            <div className="card-header"> Add Client </div>
+            <div className="card-body">
+                <form> 
+                    <div className="form-group">
+                        <label htmlFor="firstName"> First Name </label>
+                        <input 
+                            type="text"
+                            className="form-control"
+                            name="firstName"
+                            minLength="2"
+                            required
+                        /> 
+                    </div>
+                </form>
+            </div>
+        </div> 
+
+        
+      </div>
+    )
+  }
+}
+
+export default AddClient;
