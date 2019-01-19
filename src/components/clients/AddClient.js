f45e9589cf3f13bacf90304dbe5d61b9d2dd5fb2
derrick-long <long.derrick8@gmail.com>
2019-01-18 21:11:59 -0500

    finish add clients feature

--- src/components/clients/AddClient.js
+++ src/components/clients/AddClient.js
@@ -1,5 +1,9 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+// import { compose } from  'redux';
+// import { connect } from 'react-redux';
+import { firestoreConnect} from 'react-redux-firebase';
+import PropTypes from 'prop-types';
 
 class AddClient extends Component {
     state = {
@@ -7,9 +11,28 @@ class AddClient extends Component {
         lastname: '',
         email: '',
         phone: '',
-        balance: ' '
+        balance: ''
     }
-  render() {
+
+    onSubmit = (e) => {
+        e.preventDefault();
+        
+        const newClient = this.state;
+
+        const { firestore } = this.props;
+
+        if(newClient.balance === '') {
+            newClient.balance = 0;
+        }
+
+        firestore.add({ collection: 'clients' }, newClient).then(()=> 
+        this.props.history.push('/'));
+    };
+
+    onChange = (e) => this.setState({ [e.target.name]: e.target.value});
+
+  
+    render() {
     return (
       <div>
           <div className="row">
@@ -23,7 +46,7 @@ class AddClient extends Component {
         <div className="card">
             <div className="card-header"> Add Client </div>
             <div className="card-body">
-                <form> 
+                <form onSubmit={this.onSubmit}> 
                     <div className="form-group">
                         <label htmlFor="firstName"> First Name </label>
                         <input 
@@ -32,8 +55,55 @@ class AddClient extends Component {
                             name="firstName"
                             minLength="2"
                             required
+                            onChange={this.onChange}
+                            value={this.state.firstName}
                         /> 
                     </div>
+                    <div className="form-group">
+                        <label htmlFor="lastName"> Last Name </label>
+                        <input 
+                            type="text"
+                            className="form-control"
+                            name="lastName"
+                            minLength="2"
+                            required
+                            onChange={this.onChange}
+                            value={this.state.lastName}
+                        /> 
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="email"> Email </label>
+                        <input 
+                            type="email"
+                            className="form-control"
+                            name="email"
+                            onChange={this.onChange}
+                            value={this.state.email}
+                        /> 
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="phone"> Phone </label>
+                        <input 
+                            type="text"
+                            className="form-control"
+                            name="phone"
+                            minLength="10"
+                            required
+                            onChange={this.onChange}
+                            value={this.state.phone}
+                        /> 
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="balance"> Balance </label>
+                        <input 
+                            type="text"
+                            className="form-control"
+                            name="balance"
+                            onChange={this.onChange}
+                            value={this.state.balance}
+                        /> 
+                    </div>
+                    <input type="submit" value="Submit" className="btn btn-primary btn-block"/>
                 </form>
             </div>
         </div> 
@@ -44,4 +114,8 @@ class AddClient extends Component {
   }
 }
 
-export default AddClient;
+AddClient.propTypes = {
+    firestore: PropTypes.object.isRequired
+};
+
+export default firestoreConnect()(AddClient);
--- src/store.js
+++ src/store.js
@@ -29,7 +29,9 @@ firebase.initializeApp(firebaseConfig);
 
 //init firestore
 
-// const firestore = firebase.firestore();
+const firestore = firebase.firestore();
+const settings = { timestampsInSnapshots: true};
+firestore.settings(settings);
 
 const  createStoreWithFirebase = compose(
     reactReduxFirebase(firebase, rrfConfig),
