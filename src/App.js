2ca22f34be273b21deb75e52aba02220c6e31853
derrick-long <long.derrick8@gmail.com>
2019-02-01 13:01:49 -0500

    add user authentication

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import  ClientDetails from './components/clients/ClientDetails';
 import  Login from './components/auth/Login';
 import { BrowserRouter  as Router, Route, Switch } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import  { UserIsAuthenticated, UserIsNotAuthenticated } from './helpers/Auth';
 import store from './store';
 
 
@@ -21,11 +22,11 @@ class App extends Component {
           
           <div className="container">
             <Switch>
-                <Route exact path="/"  component={Dashboard} />
-                <Route exact path="/client/add" component={AddClient} />
-                <Route exact path="/client/:id" component={ClientDetails} />
-                <Route exact path="/client/edit/:id" component={EditClient} />
-                <Route exact path="/login"  component={Login} />
+                <Route exact path="/"  component={UserIsAuthenticated(Dashboard)} />
+                <Route exact path="/client/add" component={UserIsAuthenticated(AddClient)} />
+                <Route exact path="/client/:id" component={UserIsAuthenticated(ClientDetails)} />
+                <Route exact path="/client/edit/:id" component={UserIsAuthenticated(EditClient)} />
+                <Route exact path="/login"  component={UserIsNotAuthenticated(Login)} />
             </Switch>
           </div>
         
